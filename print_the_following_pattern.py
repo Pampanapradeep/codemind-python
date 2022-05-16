d485121b0d02a0b12f1d4bfdc4dcbac0641c3cec
Pampanapradeep <97426625+Pampanapradeep@users.noreply.github.com>
2022-05-16 15:08:11 +0530

    n=int(input()) for i in range(n,0,-1):     for j in range(1,n+1):         if(i>=j):             print(chr(64+i),end=' ')         else:             print("",end='')     print()

---
+++ print_the_following_pattern.py
@@ -0,0 +1,8 @@
+n=int(input())
+for i in range(n,0,-1):
+    for j in range(1,n+1):
+        if(i>=j):
+            print(chr(64+i),end=' ')
+        else:
+            print("",end='')
+    print()
