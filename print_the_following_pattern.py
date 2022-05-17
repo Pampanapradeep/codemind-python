c0f4c579066a806d9740f420abb9456f5338941b
Pampanapradeep <97426625+Pampanapradeep@users.noreply.github.com>
2022-05-17 14:06:31 +0530

    a=int(input()) k=a*1 for i in range(a,0,-1):     for j in range(1,i+1):         print(j,end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,6 @@
-n=int(input())
-for i in range(n,0,-1):
-    for j in range(1,n+1):
-        if(i>=j):
-            print(chr(64+i),end=' ')
-        else:
-            print("",end='')
+a=int(input())
+k=a*1
+for i in range(a,0,-1):
+    for j in range(1,i+1):
+        print(j,end='')
     print()
